a6a52ad249857c50477a8ed617a71a017e3becd6
irina-S123 <123304775+irina-S123@users.noreply.github.com>
2025-08-31 22:12:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+    function towelSort(matrix) {
+    // Используем метод map для обхода каждой строки
+    return matrix.flatMap((row, index) => {
+        // Если индекс чётный, оставляем строку как есть
+        // Если нечётный — разворачиваем строку
+        return index % 2 === 0 ? row : row.slice().reverse();
+    });
+}
